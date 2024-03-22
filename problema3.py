747a9ac5e137b418f8e409f9dd4a84fdd68d6f32
Javier <142508383+bigalcah@users.noreply.github.com>
2024-03-22 10:45:58 -0300

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -9,5 +9,37 @@
 #         20 30 90 90 8 5 90
 #La salida debe ser
 #         [20, [8, [5, [], [], []], [], []], [], [30, [], [], [90, [], [90, [], [90, [], [], []], []], []]]]
-t = input()
-print(t)
+
+def convertirEnteros(lista):
+    nuevaLista = []
+    for elemento in lista:
+        try:
+            nuevoElemento = int(elemento)
+        except ValueError:
+            nuevoElemento = elemento
+        nuevaLista.append(nuevoElemento)
+    return tuple(nuevaLista)
+
+def arbolTrinario(numero):
+	return [numero, [], [], []]
+	
+def insertaEnArbolTrinario(arbol,numero):
+	if arbol==[]:
+		arbol+=arbolTrinario(numero)
+	elif numero < arbol[0]:
+		insertaEnArbolTrinario(arbol[1],numero)
+	elif numero == arbol[0]:
+		insertaEnArbolTrinario(arbol[2], numero)
+	else:
+		insertaEnArbolTrinario(arbol[3],numero)
+
+entrada = input().split()
+
+entradaTupla = convertirEnteros(entrada)
+
+arbol = arbolTrinario(entradaTupla[0])
+
+for elemento in entradaTupla:
+	insertaEnArbolTrinario(arbol, elemento)
+
+print(arbol)
